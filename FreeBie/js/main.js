d44b0a8063b13cee9306eed532ff7626701ad87d
Vadim <67419556+oleinikvadim@users.noreply.github.com>
2020-11-13 19:41:59 +0200

    sdf

--- FreeBie/js/main.js
+++ FreeBie/js/main.js
@@ -67,6 +67,16 @@
     }
     
     function autoPlayYouTubeModal() {
+      // var trigger = $("body").find('[data-toggle="modal"]');
+      // trigger.click(function () {
+      //     var theModal = $(this).data("target"),
+      //         videoSRC = $(this).attr("data-theVideo"),
+      //         videoSRCauto = videoSRC + "?autoplay=1";
+      //     $(theModal + ' iframe').attr('src', videoSRCauto);
+      //     $(theModal + ' button.close').click(function () {
+      //         $(theModal + ' iframe').attr('src', videoSRC);
+      //     });
+      // });
       var trigger = $("body").find('[data-toggle="modal"]');
       trigger.click(function () {
           var theModal = $(this).data("target"),
@@ -76,6 +86,11 @@
           $(theModal + ' button.close').click(function () {
               $(theModal + ' iframe').attr('src', videoSRC);
           });
+          $('.modal').click(function () {
+              $(theModal + ' iframe').attr('src', videoSRC);
+          });
+        //video play on click
+        $('.video-player-main iframe')[0].contentWindow.postMessage('{func":"playVideo":""}', '*');
       });
     }
 
